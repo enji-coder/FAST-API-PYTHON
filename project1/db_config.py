f1e9e23da80dab4d61f9d85141292c55b997feb5
enji-coder <enji.20.patel@gmail.com>
2026-02-14 10:07:46 +0530

    configuration

--- project1/db_config.py
+++ project1/db_config.py
@@ -1,16 +1,42 @@
-#--> create_engine - it create a connection between python and mysql
+
 from sqlalchemy import create_engine
-
-#--> sessionmaker : this create database session 
-
-"""
-temporary conversation with database 
-insert,update,read,.etc  without session we can't talk with database
-"""
-
-#--> declarative_base
-"""
-this is a base class that all your database models will inherit from it. 
-"""
 from sqlalchemy.orm import sessionmaker,declarative_base
 
+# ==============================
+# step : 1 Database Configuration
+# ==============================
+
+DATABASE_URL = "mysql+pymysql://root:123456789@localhost/fastapi_db1"
+
+# ==============================
+# step : 2 Creating Engine 
+# ==============================
+
+#engine creation 
+engine = create_engine(DATABASE_URL,echo=True)  # echo Shows SQL queries in terminal
+
+# ==============================
+# step : 3  Create Session
+# ===============================
+
+SessionLocal = sessionmaker(
+    autocommit = False,
+    autoflush=False,
+    bind=engine
+)
+
+# ==============================
+# step : 4  Base class for model 
+# ==============================
+Base = declarative_base()
+
+# ==============================
+# step : 5 Dependency (For FastAPI)
+# ==============================
+
+def get_db():
+    db = SessionLocal()
+    try: 
+        yield db 
+    finally:
+        db.close()
